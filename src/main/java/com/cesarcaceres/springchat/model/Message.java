5eff2fd0e75182fddc4b0578acd700b53a7c9c2f
Cesar Caceres <cesarc770@gmail.com>
2019-08-25 11:23:21 -0600

    modified message model:

--- src/main/java/com/cesarcaceres/springchat/model/Message.java
+++ src/main/java/com/cesarcaceres/springchat/model/Message.java
@@ -1,19 +1,52 @@
 package com.cesarcaceres.springchat.model;
 
 public class Message {
-    private String content;
+    private String username;
+    private String message;
+    private String type;
+    private String onlineCount;
 
-    public Message() {}
 
-    public Message(String text) {
-        this.content = text;
+    public Message() {
     }
 
-    public String getContent() {
-        return content;
+    public Message(String username, String message, String type, String onlineCount) {
+        this.username = username;
+        this.message = message;
+        this.type = type;
+        this.onlineCount = onlineCount;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(String onlineCount) {
+        this.onlineCount = onlineCount;
     }
 }
+
