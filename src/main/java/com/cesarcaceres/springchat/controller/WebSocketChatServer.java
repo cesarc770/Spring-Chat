6a95b0bf816da4e4f866e5627f96d9b07ca980f6
Cesar Caceres <cesarc770@gmail.com>
2019-08-31 00:06:54 -0600

    changed open and close to send message and update online count

--- src/main/java/com/cesarcaceres/springchat/controller/WebSocketChatServer.java
+++ src/main/java/com/cesarcaceres/springchat/controller/WebSocketChatServer.java
@@ -20,7 +20,7 @@ public class WebSocketChatServer {
     public static void sendMessageToAll(Message msg) {
         for (Map.Entry<String, Session> session : onlineSessions.entrySet()) {
             try {
-                Message toSend = new Message(msg.getUsername(), msg.getMessage(), "SPEAK" , Integer.toString(onlineSessions.size()));
+                Message toSend = new Message(msg.getUsername(), msg.getMessage(), msg.getType() , Integer.toString(onlineSessions.size()));
                 String message = JSON.toJSONString(toSend);
                 session.getValue().getBasicRemote().sendText(message);
             } catch (IOException e) {
@@ -32,17 +32,22 @@ public class WebSocketChatServer {
     @OnOpen
     public void onOpen(Session session) throws IOException {
         onlineSessions.put(session.getId(), session);
+        Message msg = new Message("Chat App", "User logged in" , "OPENED_CONNECTION", Integer.toString(onlineSessions.size()));
+        sendMessageToAll(msg);
     }
 
     @OnMessage
     public void onMessage(String JsonStr) {
         Message message = JSON.parseObject(JsonStr, Message.class);
+        message.setType("SPEAK");
         sendMessageToAll(message);
     }
 
     @OnClose
     public void onClose(Session session) throws IOException {
         onlineSessions.remove(session.getId());
+        Message msg = new Message("Chat App", "Connection lost" , "CLOSED_CONNECTION", Integer.toString(onlineSessions.size()));
+        sendMessageToAll(msg);
     }
 
     @OnError
