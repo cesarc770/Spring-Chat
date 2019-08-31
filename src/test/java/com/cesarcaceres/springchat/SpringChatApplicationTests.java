2c17a5dc93d8eb79985685fbef62fa59933cc6e3
Cesar Caceres <cesarc770@gmail.com>
2019-08-31 11:25:06 -0600

    added selenium web driver and tests with it

--- src/test/java/com/cesarcaceres/springchat/SpringChatApplicationTests.java
+++ src/test/java/com/cesarcaceres/springchat/SpringChatApplicationTests.java
@@ -1,11 +1,21 @@
 package com.cesarcaceres.springchat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,11 +23,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringChatApplicationTests {
 
 	@Autowired
+	private WebApplicationContext webApplicationContext;
 	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	WebDriver webDriver;
 
 	@Test
 	public void login() throws Exception
@@ -33,6 +51,61 @@ public class SpringChatApplicationTests {
 		this.mvc.perform(get("/home")).andDo(print()).andExpect(status().isOk())
 				.andExpect(view().name("chat"));
 
+	}
+
+	@Test
+	public void loginAndJoin () throws Exception {
+		webDriver = new HtmlUnitDriver(true);
+		webDriver.get("http://localhost:8080");
+		WebElement username = webDriver.findElement(By.id("username"));
+		username.sendKeys("Cesar");
+
+		WebElement login_form = webDriver.findElement(By.className("submit"));
+		login_form.click();
+
+
+		(new WebDriverWait(webDriver, 10)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				WebElement username1 = webDriver.findElement(By.id("username"));
+				System.out.println("username: " + username1.getText());
+				return username1.getText().equals("Cesar");
+			}
+		});
+	}
+
+	@Test
+	public void chat () throws Exception {
+		webDriver = new HtmlUnitDriver(true);
+		webDriver.get("http://localhost:8080/home?username=Cesar");
+
+		WebElement inputBox = webDriver.findElement(By.id("msg"));
+		inputBox.sendKeys("Hello");
+
+		WebElement send_btn = webDriver.findElement(By.id("send-btn"));
+		send_btn.click();
+
+		(new WebDriverWait(webDriver, 5)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				WebElement textbox1 = webDriver.findElement(By.className("msg-text"));
+				return textbox1.getText().contains("Hello");
+			}
+		});
+	}
+
+	@Test
+	public void logout () throws Exception {
+		webDriver = new HtmlUnitDriver(true);
+		webDriver.get("http://localhost:8080/home?username=Cesar");
+
+		WebElement logout_btn = webDriver.findElement(By.id("logout-btn"));
+		logout_btn.click();
+
+		(new WebDriverWait(webDriver, 5)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				WebElement login_btn = webDriver.findElement(By.className("act-but"));
+				return login_btn.isDisplayed();
+			}
+		});
 	}
 
 }
